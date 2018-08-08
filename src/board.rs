d191bac323af6b527bace2bd47348aa9b53119e3
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-08-08 18:54:50 +0530

    implement a few macros for adding back an element, cover, uncover

--- src/board.rs
+++ src/board.rs
@@ -24,6 +24,36 @@ macro_rules! remove {
     }
 }
 
+macro_rules! add_back {
+    ($self: ident, $name: ident, $dir: ident, $opp_dir: ident) => {
+        fn $name(&mut $self, a: u16) {
+            $self.$opp_dir[$self.$dir[a as usize] as usize] = a;
+            $self.$dir[$self.$opp_dir[a as usize] as usize] = a;
+        }
+    }
+}
+
+macro_rules! cover_method {
+    ($self: ident, $name: ident, $frow: ident, $fcol: ident, $op: tt) => {
+        pub fn $name(&mut $self, c: u16) {
+            $self.$frow(c);
+            let mut i = $self.down(c);
+            while i != c {
+                let j = $self.right(i);
+                let k = $self.right(j);
+                let l = $self.right(k);
+                $self.column[j as usize] $op 1;
+                $self.column[k as usize] $op 1;
+                $self.column[l as usize] $op 1;
+                $self.$fcol(j);
+                $self.$fcol(k);
+                $self.$fcol(l);
+                i = $self.down(i);
+            }
+        }
+    }
+}
+
 pub struct Board {
     up: [u16; 3240],
     down: [u16; 3240],
@@ -54,8 +84,12 @@ impl Board {
     get!(self, right);
     set!(self, set_up, up, down);
     set!(self, set_left, left, right);
+    add_back!(self, add_back_row, up, down);
+    add_back!(self, add_back_column, left, right);
     remove!(self, remove_from_column, up, down);
     remove!(self, remove_from_row, left, right);
+    cover_method!(self, cover, remove_from_row, remove_from_column, -=);
+    cover_method!(self, uncover, add_back_row, add_back_column, +=);
 
     fn horizontal(&mut self) {
         for c in 0..323 {
