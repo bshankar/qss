383d2fc505d66f369fb2ef690485f812cd775df3
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-08-09 12:49:30 +0530

    implement choose column and search

--- src/board.rs
+++ src/board.rs
@@ -9,7 +9,7 @@ macro_rules! link {
 
 macro_rules! remove {
     ($self: ident, $name: ident, $dir: ident, $opp_dir: ident) => {
-        fn $name(&mut $self, a: u16) {
+        pub fn $name(&mut $self, a: u16) {
             $self.$opp_dir[$self.$dir[a as usize] as usize] = $self.$opp_dir[a as usize];
             $self.$dir[$self.$opp_dir[a as usize] as usize] = $self.$dir[a as usize];
         }
@@ -18,7 +18,7 @@ macro_rules! remove {
 
 macro_rules! add_back {
     ($self: ident, $name: ident, $dir: ident, $opp_dir: ident) => {
-        fn $name(&mut $self, a: u16) {
+        pub fn $name(&mut $self, a: u16) {
             $self.$opp_dir[$self.$dir[a as usize] as usize] = a;
             $self.$dir[$self.$opp_dir[a as usize] as usize] = a;
         }
@@ -34,7 +34,7 @@ macro_rules! cover_method {
                 let mut j = $self.right[i as usize];
                 for _k in 0..3 {
                     $self.$fcol(j);
-                    $self.column[j as usize] $op 1;
+                    $self.sizes[$self.column[(j - 325) as usize] as usize] $op 1;
                     j = $self.right[j as usize];
                 }
                 i = $self.down[i as usize];
@@ -43,25 +43,38 @@ macro_rules! cover_method {
     }
 }
 
+macro_rules! search_helper {
+    ($self:ident, $r:ident, $dir:ident, $fn:ident) => {{
+        let mut j = $self.$dir[$r as usize];
+        let c = $self.column[j as usize];
+        for _i in 0..3 {
+            $self.$fn(c);
+            j = $self.$dir[j as usize];
+        }
+    }};
+}
+
 pub struct Board {
-    up: [u16; 3240],
-    down: [u16; 3240],
-    left: [u16; 3240],
-    right: [u16; 3240],
+    pub up: [u16; 3241],
+    pub down: [u16; 3241],
+    pub left: [u16; 3241],
+    pub right: [u16; 3241],
     column: [u16; 2916],
     sizes: [u8; 324],
-    root: u16,
+    pub solution: [u16; 3241],
+    pub root: u16,
 }
 
 impl Board {
     pub fn new() -> Self {
         let mut board = Board {
-            up: [0; 3240],
-            down: [0; 3240],
-            left: [0; 3240],
-            right: [0; 3240],
+            up: [0; 3241],
+            down: [0; 3241],
+            left: [0; 3241],
+            right: [0; 3241],
             column: [0; 2916],
             sizes: [9; 324],
+            solution: [0; 3241],
             root: 324,
         };
         board.horizontal();
@@ -70,10 +83,10 @@ impl Board {
     }
     link!(self, link_down, down, up);
     link!(self, link_right, right, left);
-    add_back!(self, add_back_row, up, down);
-    add_back!(self, add_back_column, left, right);
-    remove!(self, remove_from_column, up, down);
-    remove!(self, remove_from_row, left, right);
+    add_back!(self, add_back_row, right, left);
+    add_back!(self, add_back_column, down, up);
+    remove!(self, remove_from_column, down, up);
+    remove!(self, remove_from_row, right, left);
     cover_method!(self, cover, remove_from_row, remove_from_column, -=);
     cover_method!(self, uncover, add_back_row, add_back_column, +=);
 
@@ -109,6 +122,7 @@ impl Board {
 
                 for i in 0..4 {
                     self.link_down(boc[cs[i]], (v + i) as u16);
+                    self.column[v + i - 325] = cs[i] as u16;
                     boc[cs[i]] = (v + i) as u16;
                 }
             }
@@ -117,5 +131,41 @@ impl Board {
         for c in 0..324 {
             self.link_down(boc[c as usize], c);
         }
+    }
+
+    pub fn choose(&self) -> u16 {
+        let mut min_column = 0;
+        let mut min_size = 10;
+        let mut c = self.right[self.root as usize];
+        while c != self.root {
+            if self.sizes[c as usize] == 1 {
+                return min_column;
+            }
+            if self.sizes[c as usize] < min_size {
+                min_size = self.sizes[c as usize];
+                min_column = c;
+            }
+            c = self.right[c as usize];
+        }
+        min_column
+    }
+
+    pub fn search(&mut self, k: u32) {
+        if self.right[self.root as usize] == self.root {
+            println!("solved!");
+            return;
+        }
+
+        let c = self.choose();
+        self.cover(c);
+        let mut r = self.down[c as usize];
+        while r != c {
+            self.solution[k as usize] = r;
+            search_helper!(self, r, right, cover);
+            self.search(k + 1);
+            search_helper!(self, r, left, uncover);
+            r = self.down[r as usize];
+        }
+        self.uncover(c);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod board;
 use board::Board;
 
+macro_rules! traverse {
+    ($b:ident, $dir:ident, $s:expr) => {{
+        let mut i = $b.$dir[$s as usize];
+        while i != $s {
+            println!("{}", i);
+            i = $b.$dir[i as usize];
+        }
+    }};
+}
+
 fn main() {
-    let b = Board::new();
-    // println!("{}", b.down(b.up(0)));
+    let mut b = Board::new();
+    let start = 449;
+    traverse!(b, right, start);
 }
